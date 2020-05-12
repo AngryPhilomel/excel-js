ab1f4a8cf5bb0e657bd11fb63cdf522a410b6846
AngryPhilomel <an.aries@icloud.com>
2020-05-12 12:50:04 +0300

    Finish base framework functional

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -1,7 +1,20 @@
-import {DomListener} from '@core/DomListener';
+import {DomListener} from '@core/DomListener'
 
 export class ExcelComponent extends DomListener {
-  toHtml() {
+  constructor($root, options = {}) {
+    super($root, options.listeners)
+    this.name = options.name
+  }
+
+  toHTML() {
     return ''
+  }
+
+  init() {
+    this.initDOMListeners()
+  }
+
+  destroy() {
+    this.removeDOMListeners()
   }
 }
